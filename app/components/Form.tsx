73c35db2501b6d321ed6ce7eaadfee7b3e67b14c
Albatraoz12 <dimosthenis.emmanouil@chasacademy.se>
2024-11-12 21:00:18 +0100

    refactorying code

--- app/components/Form.tsx
+++ app/components/Form.tsx
@@ -1,37 +1,21 @@
 'use client';
 import Link from 'next/link';
 import React, { useState } from 'react';
-import { redirect } from 'next/navigation';
 import { signIn } from 'next-auth/react';
 
 const Form = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [formData, setFormData] = useState({
-    email: email,
-    password: password,
-  });
+  const [formData, setFormData] = useState({ email: '', password: '' });
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
 
   const handleSignIn = async (e: React.FormEvent) => {
     e.preventDefault();
-    const result = await signIn('credentials', {
-      redirect: false, // Set to false to handle redirection manually
-      email: formData.email,
-      password: formData.password,
+    await signIn('credentials', {
+      ...formData,
+      redirect: true,
+      callbackUrl: '/dashboard',
     });
-
-    if (result?.ok) {
-      // Redirect to dashboard on successful login
-      redirect('/dashboard');
-    } else {
-      // Handle login error (e.g., display error message)
-      alert('Login failed');
-    }
   };
 
   return (
@@ -57,7 +41,6 @@ const Form = () => {
             name='email'
             placeholder='John@doe.com'
             onChange={handleChange}
-            // onChange={(e) => setUsername(e.target.value)}
             value={formData.email}
             className='mt-1 p-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-green-500 dark:bg-gray-700 dark:border-gray-600 dark:text-gray-200 dark:placeholder-gray-400'
           />
@@ -75,7 +58,6 @@ const Form = () => {
             id='password'
             name='password'
             placeholder='Password'
-            // onChange={(e) => setPassword(e.target.value)}
             onChange={handleChange}
             value={formData.password}
             className='mt-1 p-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-green-500 dark:bg-gray-700 dark:border-gray-600 dark:text-gray-200 dark:placeholder-gray-400'
@@ -89,13 +71,6 @@ const Form = () => {
           >
             Sign in
           </button>
-          {/* <button
-            type='button'
-            className='p-2 px-4 bg-green-600 text-white rounded-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500'
-            onClick={signIn}
-          >
-            Sign in
-          </button> */}
           <Link
             href='/signup'
             className='p-2 px-4 bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-gray-200 rounded-md hover:bg-green-600 hover:text-white focus:outline-none focus:ring-2 focus:ring-green-500'
