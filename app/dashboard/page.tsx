665ea5c513aa022c85bdaf313f2e01940f25e028
Albatraoz12 <dimosthenis.emmanouil@chasacademy.se>
2024-11-12 21:10:07 +0100

    add validation if user is not signed in, return to homepage

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,23 @@
+import { signOut } from 'next-auth/react';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../api/auth/[...nextauth]/route';
 import React from 'react';
+import SignOut from '../components/SignOut';
+import { redirect } from 'next/navigation';
 
-const page = () => {
-  return <div>Dashboard Page!!!</div>;
+const Page = async () => {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    redirect('/');
+  }
+
+  return (
+    <main>
+      <div>Welcome to Dashboard Page</div>
+      <SignOut />
+    </main>
+  );
 };
 
-export default page;
+export default Page;
