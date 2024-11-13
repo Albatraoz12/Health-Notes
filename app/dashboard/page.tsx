dde8bd95254125a970d6e291360a7ee8b7d0ce40
Albatraoz12 <dimosthenis.emmanouil@chasacademy.se>
2024-11-13 22:41:11 +0100

    implement logic to fetch user details from api and checks if user is authenticated

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,23 +1,67 @@
-import { signOut } from 'next-auth/react';
-import { getServerSession } from 'next-auth';
-import { authOptions } from '../api/auth/[...nextauth]/route';
-import React from 'react';
+'use client';
+
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { useSession } from 'next-auth/react';
 import SignOut from '../components/SignOut';
-import { redirect } from 'next/navigation';
 
-const Page = async () => {
-  const session = await getServerSession(authOptions);
+const Page = () => {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+  const [userData, setUserData] = useState<any>(null);
 
-  if (!session) {
-    redirect('/');
+  useEffect(() => {
+    if (status === 'unauthenticated') {
+      router.push('/');
+    }
+  }, [status, router]);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const res = await fetch('/api/user', {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          credentials: 'include',
+        });
+
+        if (!res.ok) {
+          throw new Error('Failed to fetch user data');
+        }
+
+        const data = await res.json();
+        setUserData(data);
+      } catch (error) {
+        console.error('Error fetching user data:', error);
+      }
+    };
+
+    if (status === 'authenticated') {
+      fetchUserData();
+    }
+  }, [status]);
+
+  if (status === 'loading') {
+    return <div>Loading...</div>;
   }
 
-  return (
-    <main>
-      <div>Welcome to Dashboard Page</div>
-      <SignOut />
-    </main>
-  );
+  if (status === 'authenticated') {
+    return (
+      <main>
+        <div>Welcome to Dashboard Page</div>
+        {userData && (
+          <div>
+            <h2>User Details:</h2>
+            <p>Email: {userData.firstName}</p>
+          </div>
+        )}
+        <SignOut />
+      </main>
+    );
+  }
+
+  return null;
 };
 
 export default Page;
