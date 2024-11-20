a29228bc56e4ef0ae565a40cc1c32aedf48cd84c
Albatraoz12 <dimosthenis.emmanouil@chasacademy.se>
2024-11-20 21:53:43 +0100

    add styling to logout button

--- app/components/SignOut.tsx
+++ app/components/SignOut.tsx
@@ -3,7 +3,14 @@ import { signOut } from 'next-auth/react';
 import React from 'react';
 
 const SignOut = () => {
-  return <button onClick={() => signOut()}>Sign out</button>;
+  return (
+    <button
+      className='p-2 px-4 bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-gray-200 rounded-md hover:bg-green-600 hover:text-white focus:outline-none focus:ring-2 focus:ring-green-500'
+      onClick={() => signOut()}
+    >
+      Sign out
+    </button>
+  );
 };
 
 export default SignOut;
